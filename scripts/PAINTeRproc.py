dd5aaf3478ca3d2bc1194b17c711a54d38aa1fd9
Tamas Spisak <spisak.tms@gmail.com>
2018-08-20 12:01:59 +0200

    major refactoring of machine learning code - take 2

---
+++ scripts/PAINTeRproc.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+import sys
+# sys.path.append("/home/balint/Dokumentumok/phd/github/") #PUMI should be added to the path by install or by the developer
+# az importalasnal az ~_preproc utan a .fajlnev-et kell megadni
+import nipype
+import nipype.pipeline as pe
+# import the defined workflows from the anat_preproc folder
+import nipype.interfaces.io as nio
+import nipype.interfaces.fsl as fsl
+import nipype.interfaces.afni as afni
+import PUMI.AnatProc as anatproc
+import PUMI.FuncProc as funcproc
+# import the necessary workflows from the func_preproc folder
+import PUMI.anat_preproc.Func2Anat as bbr
+import PUMI.func_preproc.func2standard as transform
+import PUMI.utils.utils_convert as utils_convert
+import os
+import PUMI.utils.globals as globals
+import PUMI.connectivity.TimeseriesExtractor as tsext
+import PUMI.connectivity.NetworkBuilder as nw
+#import PUMI.utils.addimages as adding
+
+# parse command line arguments
+if (len(sys.argv) <= 2):
+    print("Please specify command line arguments!")
+    print("Usage:")
+    print(sys.argv[0] + " <\"highres_data_template\"> <\"func_data_template\"> [results_sink_directory]")
+    print("Example:")
+    print(sys.argv[0] + " \"highres_data/subject_*.nii.gz\" \"func_data/subject_*.nii.gz\"")
+    quit()
+
+if (len(sys.argv) > 3):
+    globals._SinkDir_ = sys.argv[3]
+
+if (len(sys.argv) > 4):
+    _MISTDIR_=sys.argv[4]
+else:
+    _MISTDIR_ = '/home/analyser/Documents/mistatlases/'
+
+##############################
+#_regtype_ = globals._RegType_.FSL
+_regtype_ = globals._RegType_.ANTS
+globals._brainref="/data/standard/MNI152_T1_2mm_brain.nii.gz"
+globals._headref="/data/standard/MNI152_T1_2mm.nii.gz"
+globals._brainref_mask="/data/standard/MNI152_T1_2mm_brain_mask_dil.nii.gz"
+##############################
+_refvolplace_ = globals._RefVolPos_.first
+
+
+# specify atlas for network construction:
+# name of labelmap nii (or list of probmaps)
+_ATLAS_FILE = _MISTDIR_ + '/Parcellations/MIST_122.nii.gz'
+# a list of labels, where index+1 corresponds to the label in the labelmap
+_ATLAS_LABELS = tsext.mist_labels(mist_directory=_MISTDIR_, resolution="122")
+# a list of labels, where index i corresponds to the module of the i+1th region, this is optional
+_ATLAS_MODULES = tsext.mist_modules(mist_directory=_MISTDIR_, resolution="122")
+##############################
+
+# create data grabber
+datagrab = pe.Node(nio.DataGrabber(outfields=['func', 'struct']), name='data_grabber')
+
+datagrab.inputs.base_directory = os.getcwd()  # do we need this?
+datagrab.inputs.template = "*"  # do we need this?
+datagrab.inputs.field_template = dict(func=sys.argv[2],
+                                      struct=sys.argv[1])  # specified by command line arguments
+datagrab.inputs.sort_filelist = True
+
+# sink: file - idx relationship!!
+pop_id = pe.Node(interface=utils_convert.List2TxtFile,
+                     name='pop_id')
+pop_id.inputs.rownum = 0
+pop_id.inputs.out_file = "subject_IDs.txt"
+pop_id.inputs.filelist = False
+ds_id = pe.Node(interface=nio.DataSink(), name='ds_pop_id')
+ds_id.inputs.regexp_substitutions = [("(\/)[^\/]*$", "IDs.txt")]
+ds_id.inputs.base_directory = globals._SinkDir_
+
+# build the actual pipeline
+reorient_struct = pe.MapNode(fsl.utils.Reorient2Std(),
+                      iterfield=['in_file'],
+                      name="reorient_struct")
+reorient_func = pe.MapNode(fsl.utils.Reorient2Std(),
+                      iterfield=['in_file'],
+                      name="reorient_func")
+
+myanatproc = anatproc.AnatProc(stdreg=_regtype_)
+myanatproc.inputs.inputspec.bet_fract_int_thr = 0.3  # feel free to adjust, a nice bet is important!
+myanatproc.inputs.inputspec.bet_vertical_gradient = -0.3 # feel free to adjust, a nice bet is important!
+# try scripts/opt_bet.py to optimise these parameters
+
+mybbr = bbr.bbr_workflow()
+# Add arbitrary number of nii images wthin the same space. The default is to add csf and wm masks for anatcompcor calculation.
+#myadding=adding.addimgs_workflow(numimgs=2)
+add_masks = pe.MapNode(fsl.ImageMaths(op_string=' -add'),
+                       iterfield=['in_file', 'in_file2'],
+                       name="addimgs")
+
+erode_mask = pe.MapNode(fsl.ErodeImage(),
+                        iterfield=['in_file'],
+                        name="erode_compcor_mask")
+
+def pickindex(vec, i):
+    return [x[i] for x in vec]
+
+myfuncproc = funcproc.FuncProc(stdrefvol=_refvolplace_)
+
+#create atlas matching this space
+resample_atlas = pe.Node(interface=afni.Resample(outputtype = 'NIFTI_GZ',
+                                          in_file=_MISTDIR_ + "/Parcellations/MIST_7.nii.gz",
+                                          master=globals._FSLDIR_ + '/data/standard/MNI152_T1_3mm_brain.nii.gz'),
+                         name='resample_atlas') #default interpolation is nearest neighbour
+
+# standardize what you need
+myfunc2mni = transform.func2mni(stdreg=_regtype_, carpet_plot="1_original", wf_name="func2mni_1")
+#myfunc2mni_cc = transform.func2mni(stdreg=_regtype_, carpet_plot="2_cc", wf_name="func2mni_2_cc")
+#myfunc2mni_cc_bpf = transform.func2mni(stdreg=_regtype_, carpet_plot="3_cc_bpf", wf_name="func2mni_3_cc_bpf")
+#myfunc2mni_cc_bpf_cens = transform.func2mni(stdreg=_regtype_, carpet_plot="4_cc_bpf_cens", wf_name="func2mni_4_cc_bpf_cens")
+myfunc2mni_cc_bpf_cens_mac = transform.func2mni(stdreg=_regtype_, carpet_plot="5_cc_bpf_cens_mac", wf_name="func2mni_5_cc_bpf_cens_mac")
+
+###################
+
+#create matrices
+myextract = tsext.extract_timeseries()
+myextract.inputs.inputspec.atlas_file = _ATLAS_FILE
+myextract.inputs.inputspec.labels = _ATLAS_LABELS
+myextract.inputs.inputspec.modules = _ATLAS_MODULES
+
+measure = "tangent"
+mynetmat = nw.build_netmat(wf_name=measure.replace(" ", "_"))
+mynetmat.inputs.inputspec.measure = measure
+
+
+totalWorkflow = nipype.Workflow('preprocess_all')
+totalWorkflow.base_dir = '.'
+
+# anatomical part and func2anat
+totalWorkflow.connect([
+    (datagrab, pop_id,
+     [('func', 'in_list')]),
+    (pop_id, ds_id,
+     [('txt_file', 'subjects')]),
+    (datagrab, reorient_struct,
+     [('struct', 'in_file')]),
+    (reorient_struct, myanatproc,
+     [('out_file', 'inputspec.anat')]),
+    (reorient_struct, mybbr,
+     [('out_file', 'inputspec.skull')]),
+    (datagrab, reorient_func,
+     [('func', 'in_file')]),
+    (reorient_func, mybbr,
+     [('out_file', 'inputspec.func')]),
+    (myanatproc, mybbr,
+      [('outputspec.probmap_wm', 'inputspec.anat_wm_segmentation'),
+       ('outputspec.probmap_csf', 'inputspec.anat_csf_segmentation'),
+       ('outputspec.probmap_gm', 'inputspec.anat_gm_segmentation'),
+       ('outputspec.probmap_ventricle', 'inputspec.anat_ventricle_segmentation')])
+
+    ])
+
+# functional part
+totalWorkflow.connect([
+    (reorient_func, myfuncproc,
+     [('out_file', 'inputspec.func')]),
+    (mybbr, add_masks,
+     [('outputspec.ventricle_mask_in_funcspace','in_file'),
+      ('outputspec.wm_mask_in_funcspace','in_file2')]),
+    (add_masks, erode_mask,
+     [('out_file','in_file')]),
+    (erode_mask, myfuncproc,
+     [('out_file','inputspec.cc_noise_roi')]),
+
+    # push func to standard space
+#    (myfuncproc, myfunc2mni,
+#    [('outputspec.func_mc', 'inputspec.func'),
+#     ('outputspec.FD', 'inputspec.confounds')]),
+#   (mybbr, myfunc2mni,
+#    [('outputspec.func_to_anat_linear_xfm', 'inputspec.linear_reg_mtrx')]),
+#   (myanatproc, myfunc2mni,
+#    [('outputspec.anat2mni_warpfield', 'inputspec.nonlinear_reg_mtrx'),
+#     #('outputspec.std_template', 'inputspec.reference_brain'),
+#     ('outputspec.brain', 'inputspec.anat')]),
+#   (resample_atlas, myfunc2mni,
+#    [('out_file', 'inputspec.atlas')]),
+
+    (myfuncproc, myfunc2mni_cc_bpf_cens_mac,
+     [('outputspec.func_mc_nuis_bpf_cens_medang', 'inputspec.func'),
+      ('outputspec.FD', 'inputspec.confounds')]),
+    (mybbr, myfunc2mni_cc_bpf_cens_mac,
+     [('outputspec.func_to_anat_linear_xfm', 'inputspec.linear_reg_mtrx')]),
+    (myanatproc, myfunc2mni_cc_bpf_cens_mac,
+     [('outputspec.anat2mni_warpfield', 'inputspec.nonlinear_reg_mtrx'),
+      #('outputspec.std_template', 'inputspec.reference_brain'),
+      ('outputspec.brain', 'inputspec.anat')]),
+    (resample_atlas, myfunc2mni_cc_bpf_cens_mac,
+     [('out_file', 'inputspec.atlas')])
+    ])
+
+# connect network analysis part
+totalWorkflow.connect(myfunc2mni_cc_bpf_cens_mac, 'outputspec.func_std', myextract, 'inputspec.std_func')
+totalWorkflow.connect(myextract, 'outputspec.timeseries_file', mynetmat, 'inputspec.timeseries')
+totalWorkflow.connect(myextract, 'outputspec.reordered_modules', mynetmat, 'inputspec.modules')
+totalWorkflow.connect(myextract, 'outputspec.relabelled_atlas_file', mynetmat, 'inputspec.atlas')
+
+# RUN #
+#from nipype.utils.profiler import log_nodes_cb
+#import logging
+#callback_log_path = 'run_stats.log'
+#logger = logging.getLogger('callback')
+#logger.setLevel(logging.DEBUG)
+#handler = logging.FileHandler(callback_log_path)
+#logger.addHandler(handler)
+totalWorkflow.write_graph('graph-orig.dot', graph2use='orig', simple_form=True)
+totalWorkflow.write_graph('graph-exec-detailed.dot', graph2use='exec', simple_form=False)
+totalWorkflow.write_graph('graph.dot', graph2use='colored')
+totalWorkflow.run(plugin='MultiProc')
+
+#from nipype.utils.draw_gantt_chart import generate_gantt_chart
+#generate_gantt_chart('run_stats.log', cores=8)
---
+++ scripts/train_on_bochum3.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import pyplot
+
+from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.metrics import r2_score, explained_variance_score, mean_squared_error
+
+import PAINTeR.load_project_data as load
+import PAINTeR.models as models
+import PAINTeR.model_selection as modsel
+from sklearn.model_selection import LeaveOneOut
+
+y = load.load_QST_data()  # default load Bochum "QST_pain_sesnitiviy" from QST data
+ts = load.load_timeseries_sav()
+X = load.compute_connectivity(ts, kind="tangent")
+
+# eliminate NA-s
+X = X[~np.isnan(y)]
+y = y[~np.isnan(y)]
+
+mymodel, p_grid = models.pipe_scale_fsel_model()
+#mymodel, p_grid = models.pipe_scale_fsel_ridge()
+
+
+p_grid = {'fsel__k': [20, 30, 35, 40, 50], 'model__alpha': [.01, .05, .1], 'model__l1_ratio': [.1, .3, .5, .8, .9]}
+#p_grid = {'fsel__k': [20, 30, 35, 40, 50], 'model__alpha': [.01, .05, .1]} # for pure ridge
+############ hyperparamter tuning with gridserach and model evaluation ####################
+
+
+k = 15  # number of folds
+
+#inner_cv = modsel.RepeatedKFold(n_splits=k, n_repeats=50)
+#outer_cv = modsel.KFold(n_splits=k)
+#inner_cv = modsel.RepeatedSortedStratifiedKFold(n_splits=k, n_repeats=15)
+#outer_cv = modsel.RepeatedSortedStratifiedKFold(n_splits=k, n_repeats=2)
+inner_cv = LeaveOneOut()
+outer_cv = LeaveOneOut()
+
+#mymodel.fit(X, y)
+#print(cross_val_score(mymodel.predict(X), y))
+
+clf = GridSearchCV(estimator=mymodel, param_grid=p_grid, cv=inner_cv, scoring="neg_mean_squared_error", verbose=False, return_train_score=False, n_jobs=8)
+
+clf.fit(X, y)
+
+
+print "*** Score on mean as model: " + str(-mean_squared_error(np.repeat(y.mean(), len(y)), y))
+print "**** Non-nested analysis ****"
+print "** Best hyperparameters: " + str(clf.best_params_)
+
+print "** Score on full data as training set:\t" + str(-mean_squared_error(y_pred=clf.best_estimator_.predict(X), y_true=y))
+print "** Best Non-nested cross-validated score on test:\t" + str(clf.best_score_)
+
+
+print "**** Nested analysis ****"
+
+
+#nested_scores = cross_val_score(clf, X, y, cv=outer_cv, scoring="explained_variance")
+#print "** Nested Score on test:\t" + str(nested_scores.mean())
+# this above has the same output as this below:
+
+best_params = []
+predicted = np.zeros(len(y))
+actual = np.zeros(len(y))
+nested_scores_train = np.zeros(outer_cv.get_n_splits(X))
+nested_scores_test = np.zeros(outer_cv.get_n_splits(X))
+nested_scores_test2 = np.zeros(outer_cv.get_n_splits(X))
+i = 0
+# doing the crossval itewrations manually
+print "model\tinner_cv mean score\touter vc score"
+for train, test in outer_cv.split(X, y):
+    clf.fit(X[train], y[train])
+    # plot histograms to check distributions
+    #bins = np.linspace(-1.5, 1.5, 6)
+    #pyplot.hist(y[train], bins, alpha=0.5, label='train')
+    #pyplot.hist(y[test], bins, alpha=0.5, label='test')
+    #pyplot.legend(loc='upper right')
+    #pyplot.show()
+
+    print str(clf.best_params_) + " " + str(clf.best_score_) + " " + str(clf.score(X[test], y[test]))
+    predicted[i] = clf.predict(X[test])
+    actual[i] = y[test]
+
+    best_params.append(clf.best_params_)
+    nested_scores_train[i] = clf.best_score_
+    nested_scores_test[i] = clf.score(X[test], y[test])
+    # clf.score is the same as calculating the score to the prediced values of the test dataset:
+    #nested_scores_test2[i] = explained_variance_score(y_pred=clf.predict(X[test]), y_true=y[test])
+    i = i+1
+
+print "*** Score on mean as model:\t" + str(-mean_squared_error(np.repeat(y.mean(), len(y)), y))
+print "** Mean score in the inner crossvaludation (inner_cv):\t" + str(nested_scores_train.mean())
+print "** Mean Nested Crossvalidation Score (outer_cv):\t" + str(nested_scores_test.mean())
+
+print "Explained Variance: " +  str( 1- nested_scores_test.mean()/-mean_squared_error(np.repeat(y.mean(), len(y)), y) )
+
+#plot the prediction of the outer cv
+fig, ax = plt.subplots()
+ax.scatter(actual, predicted, edgecolors=(0, 0, 0))
+ax.plot([y.min(), y.max()],
+           [y.min(), y.max()],
+           'k--',
+           lw=2)
+ax.set_xlabel('Pain Sensitivity')
+ax.set_ylabel('Predicted (Nested LOO)')
+plt.title("Expl. Var.:" +  str( 1- nested_scores_test.mean()/-mean_squared_error(np.repeat(y.mean(), len(y)), y) ) )
+plt.show()
+
+
