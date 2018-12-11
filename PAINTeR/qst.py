f322290d74b4743182de267398066cf09d7f7bb7
Tamas Spisak <spisak.tms@gmail.com>
2018-12-11 16:40:36 +0100

    data frames ion csv format added

--- PAINTeR/qst.py
+++ PAINTeR/qst.py
@@ -25,6 +25,18 @@ def composite_pain_sensitivity(CPT, HPT, MPT):
     # Zunhammer, M. et al.(2016) Combined glutamate and glutamine levels in pain-processing brain regions are
     # associated with individual pain sensitivity, Pain, 157(10), pp.2248-2256.
     # DOI:10.1097/j.pain.0000000000000634
-    return 0
+    return (+ (CPT-np.nanmean(CPT))/np.nanstd(CPT)\
+           - (HPT-np.nanmean(HPT))/np.nanstd(HPT)\
+           - (MPT-np.nanmean(MPT))/np.nanstd(MPT) ) / 3
 
+
+
+def composite_pain_sensitivity_PAINTeR(CPT, HPT, MPT, CPT_ex, HPT_ex, MPT_ex):
+    # calculates composite pain sensitivity score, based on the passed example sample (Bochum sample expected)
+    # Zunhammer, M. et al.(2016) Combined glutamate and glutamine levels in pain-processing brain regions are
+    # associated with individual pain sensitivity, Pain, 157(10), pp.2248-2256.
+    # DOI:10.1097/j.pain.0000000000000634
+    return (+ (CPT - np.nanmean(CPT_ex)) / np.nanstd(CPT_ex) \
+            - (HPT - np.nanmean(HPT_ex)) / np.nanstd(HPT_ex) \
+            - (MPT - np.nanmean(MPT_ex)) / np.nanstd(MPT_ex)) / 3
 
--- scripts/process_tables.py
+++ scripts/process_tables.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import pandas as pd
+import numpy as np
 from PAINTeR import global_vars
 from PAINTeR import misc
 from PAINTeR import qst
@@ -16,21 +17,46 @@ df_essen['BMI']=misc.bmi(df_essen['height'].values, df_essen['weight'].values)
 df_szeged['BMI']=misc.bmi(df_szeged['height'].values, df_szeged['weight'].values)
 
 # calculate mean QST thresholds
-qst.CPT( df_essen[['qst_cpt_2', 'qst_cpt_3', 'qst_cpt_4', 'qst_cpt_5', 'qst_cpt_6']].values, truncate=True)
-qst.HPT( df_essen[['qst_hpt_2', 'qst_hpt_3', 'qst_hpt_4', 'qst_hpt_5', 'qst_hpt_6']].values)
-qst.MPT( df_essen[['qst_mpt_1_pain', 'qst_mpt_1_no_pain',    # skip first?
+df_essen['CPT'] = qst.CPT( df_essen[['qst_cpt_2', 'qst_cpt_3', 'qst_cpt_4', 'qst_cpt_5', 'qst_cpt_6']].values,
+                           truncate=True)
+df_essen['HPT'] = qst.HPT( df_essen[['qst_hpt_2', 'qst_hpt_3', 'qst_hpt_4', 'qst_hpt_5', 'qst_hpt_6']].values)
+df_essen['MPT_log_geom'] = qst.MPT( df_essen[['qst_mpt_1_pain', 'qst_mpt_1_no_pain',    # skip first?
                    'qst_mpt_2_pain', 'qst_mpt_2_no_pain',
                    'qst_mpt_3_pain', 'qst_mpt_3_no_pain',
                    'qst_mpt_4_pain', 'qst_mpt_4_no_pain',
                    'qst_mpt_5_pain', 'qst_mpt_5_no_pain']].values)
 
-qst.CPT( df_szeged[['qst_cpt_2', 'qst_cpt_3', 'qst_cpt_4', 'qst_cpt_5', 'qst_cpt_6']].values, truncate=True)
-qst.HPT( df_szeged[['qst_hpt_2', 'qst_hpt_3', 'qst_hpt_4', 'qst_hpt_5', 'qst_hpt_6']].values)
-qst.MPT( df_szeged[['qst_mpt_1_pain', 'qst_mpt_1_no_pain',    # skip first?
+df_szeged['CPT'] = qst.CPT( df_szeged[['qst_cpt_2', 'qst_cpt_3', 'qst_cpt_4', 'qst_cpt_5', 'qst_cpt_6']].values,
+                            truncate=True)
+df_szeged['HPT'] = qst.HPT( df_szeged[['qst_hpt_2', 'qst_hpt_3', 'qst_hpt_4', 'qst_hpt_5', 'qst_hpt_6']].values)
+df_szeged['MPT_log_geom'] = qst.MPT( df_szeged[['qst_mpt_1_pain', 'qst_mpt_1_no_pain',    # skip first?
                    'qst_mpt_2_pain', 'qst_mpt_2_no_pain',
                    'qst_mpt_3_pain', 'qst_mpt_3_no_pain',
                    'qst_mpt_4_pain', 'qst_mpt_4_no_pain',
                    'qst_mpt_5_pain', 'qst_mpt_5_no_pain']].values)
+
+
+# calculate composite QST pain sensitivity (basedd on bochum sample mean and standard deviation)
+print "Mean deviation from original pain sensitivity score:"
+print np.mean(df_bochum['mean_QST_pain_sensitivity'] - qst.composite_pain_sensitivity(df_bochum['CPT'].values,
+                                                                                      df_bochum['HPT'].values,
+                                                                                      df_bochum['MPT_log_geom'].values))
+
+df_essen['mean_QST_pain_sensitivity'] = qst.composite_pain_sensitivity_PAINTeR( df_essen['CPT'].values,
+                                                                                df_essen['HPT'].values,
+                                                                                df_essen['MPT_log_geom'].values,
+                                                                                # reference:
+                                                                                df_bochum['CPT'].values,
+                                                                                df_bochum['HPT'].values,
+                                                                                df_bochum['MPT_log_geom'].values)
+df_szeged['mean_QST_pain_sensitivity'] = qst.composite_pain_sensitivity_PAINTeR(df_szeged['CPT'].values,
+                                                                                df_szeged['HPT'].values,
+                                                                                df_szeged['MPT_log_geom'].values,
+                                                                                # reference:
+                                                                                df_bochum['CPT'].values,
+                                                                                df_bochum['HPT'].values,
+                                                                                df_bochum['MPT_log_geom'].values)
+
 
 # save data frames
 df_bochum.to_csv(global_vars._RES_BOCHUM_TABLE_)
