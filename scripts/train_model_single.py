ff48890e038020d1fd119443c6e32ccba370dcff
Tamas Spisak <spisak.tms@gmail.com>
2019-09-27 10:19:36 +0200

    recalculated (no channge)

---
+++ scripts/train_model_single.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+import pandas as pd
+import numpy as np
+from sklearn.externals import joblib
+from PAINTeR import global_vars
+import PAINTeR.connectivity as conn
+from PAINTeR import plot
+from PAINTeR import model
+
+# load pain sensitivity data (excluded)
+y = pd.read_csv(global_vars._RES_BOCHUM_TABLE_EXCL_)['HPT']
+
+# load features (excluded)
+X = joblib.load(global_vars._FEATURE_BOCHUM_)
+
+X=X[~np.isnan(y)]
+y=y[~np.isnan(y)]
+
+# define model
+mymodel, p_grid = model.pipe_scale_fsel_elnet()
+
+# train model with cross-validation
+m, avg_model, all_models = model.train(X, y, mymodel, p_grid, nested=False)
+
+# serialise model
+#joblib.dump(m, global_vars._RES_PRED_MOD_)
